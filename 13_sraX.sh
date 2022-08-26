e69ee22caba589f50f111813b168ec9bb92fb78a
frankharders <frank.harders@wur.nl>
2022-08-26 16:14:45 +0200

    Create 13_sraX.sh

---
+++ 13_sraX.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+##  activate the environment for this downstream analysis
+eval "$(conda shell.bash hook)";
+conda activate amr-sraX;
+
+
+#create several directories
+while getopts "w:m:l:n:r:q:o:" opt; do
+  case $opt in
+    w)
+      #echo "-w was triggered! $OPTARG"
+      WORKDIR="`echo $(readlink -m $OPTARG)`"
+      #echo $WORKDIR
+      ;;
+     a)
+      #echo "-a was triggered! $OPTARG"
+      RAW_FASTQ="`echo $(readlink -m $OPTARG)`"
+      #echo $RAW_FASTQ
+      ;;
+     b)
+      #echo "-b was triggered! $OPTARG"
+      RAWSTATS="`echo $(readlink -m $OPTARG)`"
+      #echo $RAWSTATS
+      ;;
+	 c)
+      #echo "-c was triggered! $OPTARG"
+      POLISHED="`echo $(readlink -m $OPTARG)`"
+      #echo $POLISHED
+      ;;
+	 d)
+      #echo "-d was triggered! $OPTARG"
+      TRIMMEDSTATS="`echo $(readlink -m $OPTARG)`"
+      #echo $TRIMMEDSTATS
+      ;;
+	 e)
+      #echo "-e was triggered! $OPTARG"
+      SHOVILL="`echo $(readlink -m $OPTARG)`"
+      #echo $SHOVILL
+      ;;
+	 f)
+      #echo "-f was triggered! $OPTARG"
+      QUAST="`echo $(readlink -m $OPTARG)`"
+      #echo $QUAST
+      ;;
+	 g)
+      #echo "-g was triggered! $OPTARG"
+      QUASTparse="`echo $(readlink -m $OPTARG)`"
+      #echo $QUASTparse
+      ;;
+     h)
+      #echo "-h was triggered! $OPTARG"
+      MLST="`echo $(readlink -m $OPTARG)`"
+      #echo $MLST
+      ;;
+     i)
+      #echo "-i was triggered! $OPTARG"
+      MLSTparse="`echo $(readlink -m $OPTARG)`"
+      #echo $MLSTparse
+      ;;	  
+     j)
+      #echo "-i was triggered! $OPTARG"
+      ABRICATE="`echo $(readlink -m $OPTARG)`"
+      #echo $ABRICATE
+      ;;
+	 l)
+      #echo "-l was triggered! $OPTARG"
+      TMP="`echo $(readlink -m $OPTARG)`"
+      #echo $TMP
+      ;;
+	 m)
+      #echo "-n was triggered! $OPTARG"
+      GENOMES="`echo $(readlink -m $OPTARG)`"
+      #echo $GENOMES
+      ;;
+	 n)
+      #echo "-n was triggered! $OPTARG"
+      LOG="`echo $(readlink -m $OPTARG)`"
+      #echo $LOG
+      ;;
+	 o)
+      #echo "-n was triggered! $OPTARG"
+      SRAX="`echo $(readlink -m $OPTARG)`"
+      #echo $SRAX
+      ;;	  
+     r)
+      #echo "-r was triggered! $OPTARG"
+      REPORTING="`echo $(readlink -m $OPTARG)`"
+      #echo $REPORTING
+      ;;	  
+     s)
+      #echo "-r was triggered! $OPTARG"
+      STARAMR="`echo $(readlink -m $OPTARG)`"
+      #echo $STARAMR
+      ;;
+     t)
+      #echo "-r was triggered! $OPTARG"
+      RGI="`echo $(readlink -m $OPTARG)`"
+      #echo $RGI
+      ;;
+     q)
+      #echo "-r was triggered! $OPTARG"
+      ARCHIVE="`echo $(readlink -m $OPTARG)`"
+      #echo $ARCHIVE
+      ;;	  
+    \?)
+      echo " Let's start some analysis" >&2
+      ;;
+
+  esac
+done
+
+if [ "x" == "x$WORKDIR" ] ; then
+    echo "-w $WORKDIR "
+    echo "-w, "
+    exit 1
+fi
+
+## reporting
+GenRep="$REPORTING"/general-report.txt;
+
+
+
+
+ToDay=$(date +%Y-%m-%d-%T);
+
+dateDir="$SRAX"_"$ToDay";
+
+mkdir -p $dateDir;
+
+
+rm $GENOMES/*.log;
+rm $GENOMES/*.fai;
+
+NODES=16;
+
+sraX -a mafft -db ext -s blastx -id 95 -c 90 -t 12 -i $GENOMES -o $dateDir;
+
+
+fastaCnt=$(ls $GENOMES/*contigs.fa | wc -l);
+
+echo -e "\nsraX\nfrom $fastaCnt samples a sraX analysis is performed\n" >> "$GenRep"; 		
+	
+
+echo "sraX analysis is done using sraX for all draft genomes";
+echo -e "output files for downstream processing can be found in the directory $dateDir";
+
+
+
+exit 1
