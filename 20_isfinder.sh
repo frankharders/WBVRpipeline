7201ad84982f60c0ca41728e1039399daf7efcf3
frankharders <frank.harders@wur.nl>
2022-10-03 06:56:26 +0200

    Add files via upload

--- 20_isfinder.sh
+++ 20_isfinder.sh
@@ -8,6 +8,120 @@ conda activate amr-isescan;
 #GenRep="$REPORTING"/general-report.txt;
 
 
+while getopts "w:m:" opt; do
+  case $opt in
+    w)
+      #echo "-w was triggered! $OPTARG"
+      WORKDIR="`echo $(readlink -m $OPTARG)`"
+      #echo $WORKDIR
+      ;;
+     a)
+      #echo "-a was triggered! $OPTARG"
+      RAW_FASTQ="`echo $(readlink -m $OPTARG)`"
+      #echo $RAW_FASTQ
+      ;;
+     b)
+      #echo "-b was triggered! $OPTARG"
+      RAWSTATS="`echo $(readlink -m $OPTARG)`"
+      #echo $RAWSTATS
+      ;;
+	 c)
+      #echo "-c was triggered! $OPTARG"
+      POLISHED="`echo $(readlink -m $OPTARG)`"
+      #echo $POLISHED
+      ;;
+	 d)
+      #echo "-d was triggered! $OPTARG"
+      TRIMMEDSTATS="`echo $(readlink -m $OPTARG)`"
+      #echo $TRIMMEDSTATS
+      ;;
+	 e)
+      #echo "-e was triggered! $OPTARG"
+      SHOVILL="`echo $(readlink -m $OPTARG)`"
+      #echo $SHOVILL
+      ;;
+	 f)
+      #echo "-f was triggered! $OPTARG"
+      QUAST="`echo $(readlink -m $OPTARG)`"
+      #echo $QUAST
+      ;;
+	 g)
+      #echo "-g was triggered! $OPTARG"
+      QUASTparse="`echo $(readlink -m $OPTARG)`"
+      #echo $QUASTparse
+      ;;
+     h)
+      #echo "-h was triggered! $OPTARG"
+      MLST="`echo $(readlink -m $OPTARG)`"
+      #echo $MLST
+      ;;
+     i)
+      #echo "-i was triggered! $OPTARG"
+      MLSTparse="`echo $(readlink -m $OPTARG)`"
+      #echo $MLSTparse
+      ;;	  
+     j)
+      #echo "-i was triggered! $OPTARG"
+      ABRICATE="`echo $(readlink -m $OPTARG)`"
+      #echo $ABRICATE
+      ;;
+	 l)
+      #echo "-l was triggered! $OPTARG"
+      TMP="`echo $(readlink -m $OPTARG)`"
+      #echo $TMP
+      ;;
+	 m)
+      #echo "-n was triggered! $OPTARG"
+      GENOMES="`echo $(readlink -m $OPTARG)`"
+      #echo $GENOMES
+      ;;
+	 n)
+      #echo "-n was triggered! $OPTARG"
+      LOG="`echo $(readlink -m $OPTARG)`"
+      #echo $LOG
+      ;;
+	 o)
+      #echo "-n was triggered! $OPTARG"
+      SRAX="`echo $(readlink -m $OPTARG)`"
+      #echo $SRAX
+      ;;	  
+	 p)
+      #echo "-n was triggered! $OPTARG"
+      MUMI="`echo $(readlink -m $OPTARG)`"
+      #echo $MUMI
+      ;;	
+     r)
+      #echo "-r was triggered! $OPTARG"
+      REPORTING="`echo $(readlink -m $OPTARG)`"
+      #echo $REPORTING
+      ;;	  
+     s)
+      #echo "-r was triggered! $OPTARG"
+      STARAMR="`echo $(readlink -m $OPTARG)`"
+      #echo $STARAMR
+      ;;
+     t)
+      #echo "-r was triggered! $OPTARG"
+      RGI="`echo $(readlink -m $OPTARG)`"
+      #echo $RGI
+      ;;
+     q)
+      #echo "-r was triggered! $OPTARG"
+      ARCHIVE="`echo $(readlink -m $OPTARG)`"
+      #echo $ARCHIVE
+      ;;	  
+    \?)
+      echo " Let's start some analysis" >&2
+      ;;
+
+  esac
+done
+
+if [ "x" == "x$WORKDIR" ] ; then
+    echo "-w $WORKDIR "
+    echo "-w, "
+    exit 1
+fi
 
 count0=1;
 countS=$(cat samples.txt | wc -l);
